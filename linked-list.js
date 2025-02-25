8aad4d9e8e90b5c957e7e0bab04eab6aa14ef45a
Jordy4D_LinuxVM <jsadams27@gmail.com>
2025-02-25 08:15:28 -0500

    first commit

---
+++ linked-list.js
@@ -0,0 +1,53 @@
+class LinkedList {
+    constructor(list) {
+
+    }
+}
+
+class Node {
+    constructor(value, nextNode) {
+        this.value = null;
+        this.nextNode = null;
+    }
+
+    append(value) {
+
+    }
+
+    prepend(value) {
+
+    }
+
+    size() {
+
+    }
+
+    head() {
+
+    }
+
+    tail() {
+
+    }
+
+    at(index) {
+
+    }
+
+    pop() {
+
+    }
+
+    contains(value) {
+
+    }
+
+    find(value) {
+
+    }
+
+    toString() {
+        
+    }
+
+}
