97744c722e76b8aa9c88f28df1d9140536933575
Jordan Adams <jordan.adams@apexservicemw.com>
2025-03-01 15:28:12 -0500

    making some headway on the append method, getting close

--- linked-list.js
+++ linked-list.js
@@ -1,21 +1,30 @@
+class Node {
+    constructor(value = null) {
+        this.value = value;
+        this.nextNode = null;
+    }
+
+}
+
+
 class LinkedList {
-    constructor(list) {
-        this.head = null;
+    constructor(head = null, tail = null) {
+        this.head = head;
         this.length = 0;
-        this.tail = null;
+        this.tail = tail;
     }
 
 // remember to use recursion!!!
 
     append(value) {
-        const n = new Node(value, nextNode)
+        const n = new Node(value)
         if (this.head === null) {
-            this.head = n;
-        } else {
-            this.tail = n;
-            this.tail
-
+           this.head = n
+        } else if (this.nextNode === null) {
+          this.tail = n
         }
+    
+        this.length += 1
     }
     
     prepend(value) {
@@ -23,7 +32,7 @@ class LinkedList {
     }
     
     size() {
-    
+      console.log(this.length)
     }
     
     head() {
@@ -51,19 +60,25 @@ class LinkedList {
     }
     
     toString() {
-        
+        let current = this.head
+        while (this.head !== null) {
+          console.log(current)
+          current = current.nextNode
+        }
     }
 
 }
 
-class Node {
-    constructor(value, nextNode) {
-        this.value = null;
-        this.nextNode = null;
-    }
+const list = new LinkedList();
 
-    if (value =! null ) {
-        this.value = value;
-    }
+list.append("dog");
+list.append("cat");
+list.append("parrot");
+list.append("hamster");
+list.append("snake");
+list.append("turtle");
 
-}
+console.log(list)
+
+console.log(list.size());
+console.log(list.toString());
