d48eff07ed7643e3b25cc12e47541b3ae007c8dc
Jordan Adams <jordan.adams@apexservicemw.com>
2025-04-13 11:19:43 -0400

    node and tree classes init

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -1,1 +1,23 @@
-console.log('hi')
+class Node {
+    constructor(value = null) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class Tree {
+    constructor(array, root = null) {
+        this.root = root;
+        this.array = array
+    }
+
+    buildTree(array) {
+        this.array = array;
+        this.root = new Node(array[0])
+
+        
+       
+    }
+
+}
