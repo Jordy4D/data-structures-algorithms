bc48ac22bcfde3f27a66dae0efe8344238f133b3
Jordan Adams <jordan.adams@apexservicemw.com>
2025-04-14 08:32:24 -0400

    breaks out clean/sort array function into separate method

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -13,29 +13,24 @@ class Tree {
 
     buildTree(array) {
         const newArray = array;
-        this.root = new Node(array[0])
+        // this.root = new Node(array[0])
+        
+        // console.log(newArray)
+        
+        const cleanArray = this._sortAndDedupe(array)
+        
+        const start = 0
+        const end = cleanArray.length - 1
+        
+        
+        
         
         console.log(newArray)
+        console.log(cleanArray)
+        console.log(start, end)
 
-        const sortedArray = newArray.sort(function(a, b){ return a-b})
+        
 
-        const dedupeArr = []
-
-
-        for (let x = 0; x < sortedArray.length; x++) {
-            let current = sortedArray[x]
-            let next = sortedArray[x+1]
-            
-            if (current === next) {
-                continue;
-            } else {
-
-                dedupeArr.push(current)
-            }
-        }
-
-        console.log(sortedArray)
-        console.log(dedupeArr)
 
             // while (current > next) {
             //     let temp = current
@@ -57,6 +52,29 @@ class Tree {
         // console.log(sortedArray)
 
     }
+
+    _sortAndDedupe(array) {
+        const newArray = array
+        const sortedArray = newArray.sort(function(a, b){ return a-b})
+        const dedupeArr = []
+
+        // de-dupe array
+        for (let x = 0; x < sortedArray.length; x++) {
+            let current = sortedArray[x]
+            let next = sortedArray[x+1]
+            
+            if (current === next) {
+                continue;
+            } else {
+
+                dedupeArr.push(current)
+            }
+        }
+
+        return dedupeArr
+
+    }
+
 
 }
 
