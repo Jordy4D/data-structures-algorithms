61b7306b4241fdce14be25b716f243f3dec446d3
Jordy4D_LinuxVM <jsadams27@gmail.com>
2025-04-19 13:32:02 -0400

    working buildTree method, adds insert, delete, and find methods as well. starts levelOrder method

--- binary-search-tree.js
+++ binary-search-tree.js
@@ -11,7 +11,7 @@ class Tree {
         this.root = null;
     }
 
-    buildTree(array, start = 0, end = array.length - 1) {
+    buildTree(array, start, end) {
         // const newArray = array;
         
         // const cleanArray = this._sortAndDedupe(array)
@@ -51,9 +51,9 @@ class Tree {
 
     }
 
-    callTreeFunc(array) {
-        this.root = this.buildTree(array)
-    }
+    // callTreeFunc(array) {
+    //     this.root = this.buildTree(array)
+    // }
 
     _sortAndDedupe(array) {
         const newArray = array
@@ -77,6 +77,88 @@ class Tree {
 
     }
 
+    // for use in buildTree method
+    sortedArrToBST(arr) {
+        return this.buildTree(arr, 0, arr.length - 1)
+    }
+    
+    insert(root, value) {
+        if (root === null) {
+            return new Node(value)
+        }
+
+        if (root.value === value) {
+            return root
+        }
+
+        if (value < root.value) {
+            root.left = this.insert(root.left, value)
+        } else if (value > root.value) {
+            root.right = this.insert(root.right, value)
+        }
+
+        return root
+    }
+
+    // for use in delete method
+    getSuccessor(curr) {
+        curr = curr.right;
+        while (curr !== null && curr.left !== null) {
+            curr = curr.left;
+        }
+        return curr;
+    }
+    
+    delete(root, x) {
+        if (root === null) {
+            return root;
+        }
+
+        if (root.value > x) {
+            root.left = this.delete(root.left, x)
+        } else if (root.value < x ) {
+            root.right = this.delete(root.right, x)
+        } else {
+
+            if (root.left === null) {
+                return root.right
+            }
+
+            if (root.right === null) {
+                return root.left
+            }
+
+            let succ = this.getSuccessor(root)
+            root.value = succ.value
+            root.right = this.delete(root.right, succ.value)
+        }
+
+        return root
+    }
+
+    find(root, value) {
+        if (root === null) {
+            return null
+        }
+
+        if (root.value === value ) {
+            return root
+        }
+
+        if (root.value > value) {
+            return this.find(root.left, value)
+        } else if (root.value < value) {
+            return this.find(root.right, value)
+        } 
+
+        // console.log(root)
+        return this.root;
+    }
+    
+    levelOrder(callback) {
+        
+    }
+
     
 }
 
@@ -87,7 +169,7 @@ function prettyPrint(node, prefix = "", isLeft = true) {
     if (node.right !== null) {
       prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
     }
-    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.value}`);
     if (node.left !== null) {
       prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
     }
@@ -100,5 +182,11 @@ const sortedArr = test._sortAndDedupe(arr)
 
 
 
-test.root = test.buildTree(sortedArr) 
+test.root = test.sortedArrToBST(sortedArr) 
+
 prettyPrint(test.root)
+test.insert(test.root, 30)
+prettyPrint(test.root)
+test.delete(test.root, 23)
+prettyPrint(test.root)
+console.log(test.find(test.root, 5))
