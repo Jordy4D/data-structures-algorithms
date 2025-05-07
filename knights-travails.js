2831a6bbe411d3cd6190322fa39796ea608b5af7
Jordan Adams <jordan.adams@apexservicemw.com>
2025-05-06 22:13:52 -0400

    minor cleanup

--- knights-travails.js
+++ knights-travails.js
@@ -49,7 +49,7 @@ function knightMoves([startX, startY], [endX, endY]) {
 
         possibleMoves.forEach((move) => {
             nextNode = new Node([(currentNode[0].x + move[0]), (currentNode[0].y + move[1])])
-sds
+
 
             if (knightInBonuds(nextNode.coord) && nextNode.visited === false ) {
                 queue.push([nextNode, currentNode[1].concat([nextNode])])
@@ -66,77 +66,3 @@ sds
 
 
 knightMoves([0,0],[6,5])
-
-
-
-// console.log('while loop started')
-//         const node = path.shift()
-
-//         console.log('path is', path)
-
-//         if (node.coord === endNode.coord) {
-//             console.log('node === endNode')
-//             return visitedNodes
-//         }
-
-//         node.visited = true;
-//         visitedNodes.push(node)
-//         console.log('visited nodes is', visitedNodes)
-//         // console.log(visitedNodes)
-
-//         possibleMoves.forEach((move) => {
-//             console.log('move is ', move)
-
-//             let x = node.x + move[0]
-//             let y = node.y + move[1]
-
-//             const newNode = new Node([x, y])
-//             console.log('new node is ', newNode)
-
-            // if (newNode.x < 0 || newNode.x > 7 || newNode.y < 0 || newNode.y > 7) {
-            //     return console.log('out of bounds')
-            // } else {
-            //     path.push(newNode)
-            // }
-
-        // })
-
-        // for (const move of possibleMoves) {
-        //     console.log('for loop started')
-        //     // console.log(move)
-        //     // console.log(move[0])
-        //     // console.log(move[1])
-            
-
-        //     let x = node.x + move[0]
-        //     let y = node.y + move[1]
-            
-            
-        //     if ((x >= 0 && x <= 7 && y >= 0 && y <= 7)) {
-        //         const newNode = new Node([x, y])
-        //         console.log('new node is', newNode)
-        //         path.push(newNode)
-        //     } else {
-        //         console.log('out of bounds')
-        //         return
-        //     }
-            
-        // }
-        
-        // console.log(`visitednodes array is`, visitedNodes )
-
-        // possibleMoves.forEach((pair) => {
-
-    //     let node = new Node([x, y])
-    //     node.previousNode = startNode
-        
-    //     results.push(node)
-
-    //     // if (x <= 7 && y <= 7) {
-            
-    //     // }
-    // })
-
-
-    // console.log(startNode,endNode)
-    // console.log(path)
